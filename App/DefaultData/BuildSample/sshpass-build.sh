0e297a87ffee8829f60306ef7364cc16c1c37717
LORDofDOOM <tb@bbz-int.de>
2016-11-13 03:06:34 +0100

    [Core] Update sshpass sample, support x64

--- App/DefaultData/BuildSample/sshpass-build.sh
+++ App/DefaultData/BuildSample/sshpass-build.sh
@@ -1,7 +1,10 @@
-tar xzvf sshpass-1.05.tar.gz
-cp sshpass-1.05-1bl1.cygport sshpass-1.05/sshpass-1.05-1bl1.cygport
-cp sshpass-1.05.tar.gz sshpass-1.05/sshpass-1.05.tar.gz
-cygport ./sshpass-1.05/sshpass-1.05-1bl1.cygport all
-cp sshpass-1.05/sshpass-1.05-1bl1.i686/build/sshpass.exe /bin/sshpass.exe
-rm -rf sshpass-1.05
-rm -rf sshpass-1.05-1bl1.i686
+MACHINE_TYPE=`uname -m`
+echo $MACHINE_TYPE
+
+tar xzvf sshpass-1.06.tar.gz
+cp sshpass-1.06-1bl1.cygport sshpass-1.06/sshpass-1.06-1bl1.cygport
+cp sshpass-1.06.tar.gz sshpass-1.06/sshpass-1.06.tar.gz
+cygport ./sshpass-1.06/sshpass-1.06-1bl1.cygport all
+cp sshpass-1.06/sshpass-1.06-1bl1.$MACHINE_TYPE/build/sshpass.exe /bin/sshpass.exe
+rm -rf sshpass-1.06
+rm -rf sshpass-1.06-1bl1.$MACHINE_TYPE
